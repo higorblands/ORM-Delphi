64dba72afc14371ef89e9e3a629895a3e64b9675
higorblands <higorblandes@gmail.com>
2021-03-29 14:25:25 -0300

    ADD UORM

--- ORM.dpr
+++ ORM.dpr
@@ -3,7 +3,6 @@ program ORM;
 uses
   Vcl.Forms,
   UMainForm in 'UMainForm.pas' {FMain},
-  UORM in 'UORM.pas',
   UTypeDataFieldORM in 'Class\UTypeDataFieldORM.pas',
   UGeneralORM in 'Class\UGeneralORM.pas',
   UAlunoORM in 'Entidades\UAlunoORM.pas';
